6281b3868a31e915dea0dcc963fe5f62b36f4a6d
duke <duke@dookr.local>
2014-12-17 21:28:13 +0100

    .hs

---
+++ config/development.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export ROOT_URL="http://localhost"
+export METEOR_SETTINGS="$(cat config/settings.development.json)"
+export PORT=4000
---
+++ config/production.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export ROOT_URL="http://vps122771.ovh.net"
+export PORT="4000"
+export METEOR_SETTINGS="$(cat config/settings.production.json)"
