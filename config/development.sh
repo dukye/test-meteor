e8fa3ae88f2f3ff4c109a07004e556628a61c4e6
duke <duke@dookr.local>
2014-12-17 21:25:06 +0100

    rm

--- config/development.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# export MONGO_URL="mongodb://meteor:chrischris@ns320564.ip-176-31-255.eu:27017/meteor"
-export ROOT_URL="http://localhost"
-export METEOR_SETTINGS="$(cat config/settings.development.json)"
-export PORT=4000
--- config/production.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# export MONGO_URL="mongodb://meteor:chrischris@ns320564.ip-176-31-255.eu:27017/meteor"
-export ROOT_URL="http://vps122771.ovh.net"
-export PORT="4000"
-export METEOR_SETTINGS="$(cat config/settings.production.json)"
